f06844d8b6228d41c3f1c513dd5ecb77689a3fa1
Tufail Akram <72789060+akram1089@users.noreply.github.com>
2024-01-17 08:03:59 +0530

    helper udpated

--- home/helper.py
+++ home/helper.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 def send_forget_password_mail(email, token):
     subject = 'Your forget password link'
-    message = f'Hi , click on the link to reset your password https://super-algo.onrender.com/change_pass/{token}/'
+    message = f'Hi , click on the link to reset your password https://optionperks.com/change_pass/{token}/'
     # message = f'Hi , click on the link to reset your password http://localhost:8000/change_pass/{token}/'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
