9b9a2a59b79e76292faf142fbe211c8c4a3c0ec4
Tufail Akram <72789060+akram1089@users.noreply.github.com>
2024-03-04 09:41:06 +0530

    updated headless

--- home/views.py
+++ home/views.py
@@ -17545,7 +17545,7 @@ def update_upstox_broker(request, data):
         chrome_options.add_argument("--disable-web-security")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument('--log-level=1')
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--ignore-certificate-errors')
