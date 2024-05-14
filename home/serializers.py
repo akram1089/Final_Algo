0c8dfa0c9594cf62713975ee0f34c3e51513a64f
Tufail Akram <72789060+akram1089@users.noreply.github.com>
2024-05-14 17:08:25 +0530

    udpated serializer

--- home/serializers.py
+++ home/serializers.py
@@ -5,3 +5,20 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+
+
+
+
+
+# serializers.py
+
+from rest_framework import serializers
+
+class TemplateSerializer(serializers.Serializer):
+    content = serializers.CharField()
+
+
+
+
+
